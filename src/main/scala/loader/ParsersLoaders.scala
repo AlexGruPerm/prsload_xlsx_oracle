8b39a8029cba45feee689ff9c9f4fc4b95d50dcc
AlexGruPerm <ugr@bk.ru>
2019-09-16 12:32:34 +0500

    Refactoring parseLoadAll method with eliminating boilerplate. Rewritten on parametrized class and using High Ordered functions for parse and save into DB.

--- src/main/scala/loader/ParsersLoaders.scala
+++ src/main/scala/loader/ParsersLoaders.scala
@@ -3,9 +3,9 @@ package loader
 import java.sql.Connection
 
 import loader.CommonCCStructs._
-import loader.DBSaver._
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.slf4j.LoggerFactory
+import DBSaver._
 
 import scala.jdk.CollectionConverters._
 
@@ -49,91 +49,44 @@ class ParsersLoaders(files :Seq[FileNode], conn :Connection) {
 
 
 
-
-
-  //todo: removit
-  /*
-    val interestedMeta :String = "Заинтересованные ФОИВ"
-    log.info(s"Заинтересованные ФОИВ :${files.count(_.absName.contains(interestedMeta))} files")
-    val t1IntFoiv = System.currentTimeMillis
-    val seqAllInteresFoiv :Seq[InterestedFoiv] = files
-      //.filter(fn => fn.parentAbsName.contains("Здравоохранение"))
-      .filter(fn => fn.absName.contains(interestedMeta))
-      .flatMap(ParseInteresFoiv)
-    //seqAllCoEx.foreach(coex => log.info(coex.np_name))
-    saveInterestedFoiv(seqAllInteresFoiv)
-    log.info(s"Loaded ${seqAllInteresFoiv.size} rows")
-    log.info(s"----------------- $interestedMeta. Duration ${(System.currentTimeMillis - t1IntFoiv)} ms.")
-  */
-
-
-
   def parseLoadAll = {
     val t1 = System.currentTimeMillis
     log.info("~~~~~~~~~~~~~ Begin load all files ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     implicit val dbConn :Connection = conn
 
-    //#1. Соисполнители
-    val coexMeta :String = "Соисполнители"
-    log.info(s"$coexMeta :${files.count(_.absName.contains(coexMeta))} files")
-    val t1CoEx = System.currentTimeMillis
-    val seqAllCoEx :Seq[CoExecutor] = files.filter(fn => fn.absName.contains(coexMeta)).flatMap(ParseCoex)
-    DBSaver.saveCoExecutors(seqAllCoEx)
-    log.info(s"--------- $coexMeta. Dur: ${(System.currentTimeMillis - t1CoEx)} ms. rows = ${seqAllCoEx.size}")
+    /**
+     * Class describe meta for each source file structure.
+     * entityName is a file name part that unique identify this file from all files.
+     * isEnabled: 0,1 is this entity use in loading (mainly for debug purpose)
+     * parseFunc - function that read files found by pattern (entityName) and for each file iterate it by rows and cells.
+     * saveIntoDbFunc - function that using for save Seq of [Case class] into DB.
+     * For parseFunc and saveIntoDbFunc it's better to look internal realization.
+     *
+    */
+    case class FileLoadMeta[T](entityName :String, isEnabled :Int = 1,
+                               parseFunc: (FileNode) => Seq[T] ,
+                               saveIntoDbFunc: (Seq[T]) => Unit){
+     def load = {
+       val tBegin = System.currentTimeMillis
+       val foundFilesCount = files.count(_.absName.contains(entityName))
+       log.info(s"$entityName :$foundFilesCount files")
+       val seqEntity :Seq[T] = files.filter(fn => fn.absName.contains(entityName)).flatMap(parseFunc)
+       saveIntoDbFunc(seqEntity)
+       log.info(s"  $entityName. Dur: ${(System.currentTimeMillis - tBegin)} ms. rows = ${seqEntity.size}")
+     }
+    }
 
+    val seqLoads :Seq[FileLoadMeta[_]] = Seq(
+      FileLoadMeta[CoExecutor]("Соисполнители",1, ParseCoex, saveCoExecutors),
+      FileLoadMeta[InterestedFoiv]("Заинтересованные ФОИВ",1, ParseInteresFoiv, saveInterestedFoiv),
+      FileLoadMeta[TargetIndic]("Цели и показатели.xlsx",1, ParseTargetIndic, saveTargetIndic),
+      FileLoadMeta[ProjStruct]("Структура проекта.xlsx",1, ParsePrjStruct, saveProjStruct),
+      FileLoadMeta[TargetIndicCode]("Цели и показатели-Код",1, ParseTargetIndicCode, saveTargetIndicCode),
+      FileLoadMeta[TaskCode]("Задачи-Код",1,ParseTaskCode, saveTaskCode)
+      //,FileLoadMeta[FinancialProvision]("Финансовое обеспечение-ФБ",1,XXX)
+    )
 
-    //#2. Заинтересованные ФОИВ
-    val interestedMeta :String = "Заинтересованные ФОИВ"
-    log.info(s"$interestedMeta :${files.count(_.absName.contains(interestedMeta))} files")
-    val t1IntFoiv = System.currentTimeMillis
-    val seqAllInteresFoiv :Seq[InterestedFoiv] = files.filter(fn => fn.absName.contains(interestedMeta)).flatMap(ParseInteresFoiv)
-    saveInterestedFoiv(seqAllInteresFoiv)
-    log.info(s"--------- $interestedMeta. Dur: ${(System.currentTimeMillis - t1IntFoiv)} ms. rows = ${seqAllInteresFoiv.size} ")
-
-
-    //#3. Цели и показатели.xlsx (Без пунктов или кодов, перед .xlsx)
-    val targetIndicMeta :String = "Цели и показатели.xlsx"
-    log.info(s"$targetIndicMeta :${files.count(_.absName.contains(targetIndicMeta))} files")
-    val t1TarInd = System.currentTimeMillis
-    val seqAllTargetIndic :Seq[TargetIndic] = files.filter(fn => fn.absName.contains(targetIndicMeta)).flatMap(ParseTargetIndic)
-    saveTargetIndic(seqAllTargetIndic)
-    log.info(s"--------- $targetIndicMeta. Dur: ${(System.currentTimeMillis - t1TarInd)} ms. rows = ${seqAllTargetIndic.size}")
-
-
-    //#4. Структура проекта.xlsx
-    val projStructMeta :String = "Структура проекта.xlsx"
-    log.info(s"$projStructMeta :${files.count(_.absName.contains(projStructMeta))} files")
-    val t1PrjStruct = System.currentTimeMillis
-    val seqAllPrjStruct :Seq[ProjStruct] = files
-      .filter(fn => fn.absName.contains(projStructMeta)).flatMap(ParsePrjStruct)
-    saveProjStruct(seqAllPrjStruct)
-    log.info(s"--------- $projStructMeta. Dur: ${(System.currentTimeMillis - t1PrjStruct)} ms. rows = ${seqAllPrjStruct.size}")
-
-
-    //#5. X-Y. Цели и показатели-Код N.xlsx
-    val targetIndicCodeMeta :String = "Цели и показатели-Код"
-    log.info(s"$targetIndicCodeMeta :${files.count(_.absName.contains(targetIndicCodeMeta))} files")
-    val t1targetIndicCode = System.currentTimeMillis
-    val seqAllTargetIndicCode :Seq[TargetIndicCode] = files
-      .filter(fn => fn.absName.contains(targetIndicCodeMeta)).flatMap(ParseTargetIndicCode)
-    saveTargetIndicCode(seqAllTargetIndicCode)
-    log.info(s"--------- $targetIndicCodeMeta. Dur: ${(System.currentTimeMillis - t1targetIndicCode)} ms. rows = ${seqAllTargetIndicCode.size}")
-
-
-    //#6. X-Y. Задачи-Код Z.xlsx
-    val taskCodeMeta :String = "Задачи-Код"
-    log.info(s"$taskCodeMeta :${files.count(_.absName.contains(taskCodeMeta))} files")
-    val t1taskCode = System.currentTimeMillis
-    val seqAllTaskCode :Seq[TaskCode] = files
-      .filter(fn => fn.absName.contains(taskCodeMeta)).flatMap(ParseTaskCode)
-    saveTaskCode(seqAllTaskCode)
-    log.info(s"--------- $taskCodeMeta. Dur: ${(System.currentTimeMillis - t1taskCode)} ms. rows = ${seqAllTaskCode.size}")
-
-
-    //#7. X. Финансовое обеспечение-ФБ.xlsx
-    val FinProvisionMeta :String = "Финансовое обеспечение-ФБ"
-
-
+    seqLoads.map(_.load)
 
     val durMs = System.currentTimeMillis - t1
     log.info(s"~~~~~~~~~~~~~ End load all files. Duration $durMs ms. ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
